ece1a213e720ba882503273c657dc9452a710cbe
Chinonye Ekpendu <alexekpendu001@gmail.com>
2023-01-07 09:47:01 +0200

    substituted useState with useReducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import { useState } from 'react';
-import './App.css';
+import { useState, useReducer } from "react";
+import "./App.css";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+      case "reset":
+        return {count: 0}
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   
+
   function increment() {
-    setCount(prevCount => prevCount + 1) 
+    dispatch({ type: "increment" });
   }
 
-  function decrement(){
-    setCount(prevCount => prevCount - 1)
+  function decrement() {
+    dispatch({ type: "decrement" });
   }
 
-  function reset (){
-    setCount(0)
+  function reset() {
+    dispatch({type: "reset"})
   }
 
   return (
     <div className="App">
-      <h1>count:{count}</h1>
+      <h1>count:{state.count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button>
