df90b619496b134fee091a528779770d3ab72c9c
Chinonye Ekpendu <alexekpendu001@gmail.com>
2023-01-10 10:48:42 +0200

    half-finished styles

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>count:{state.count}</h1>
+      <h1 className="output">Count:{state.count}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset} className="span-two">Reset</button>
